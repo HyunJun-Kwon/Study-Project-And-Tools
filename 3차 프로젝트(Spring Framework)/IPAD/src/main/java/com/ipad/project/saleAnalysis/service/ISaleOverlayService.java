8599720797f207cf6f6f9b0d9e4062d27b45286e
HyunJun-Kwon <rnjsguswns5@gmail.com>
2024-03-04 16:51:58 +0900

    개발계획서 추가

--- 3차 프로젝트(Spring Framework)/IPAD/src/main/java/com/ipad/project/saleAnalysis/service/ISaleOverlayService.java
+++
@@ -1,9 +0,0 @@
-package com.ipad.project.saleAnalysis.service;
-
-import java.util.List;
-
-import com.ipad.project.saleAnalysis.model.SaleOverlayVO;
-
-public interface ISaleOverlayService {
-	List<SaleOverlayVO> getOverlay();
-}
