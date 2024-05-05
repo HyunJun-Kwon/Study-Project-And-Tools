77fe195f0793da691ba40eedb35f34814bf760f4
HyunJun-Kwon <150315701+HyunJun-Kwon@users.noreply.github.com>
2024-05-05 23:11:37 +0900

    [level 1] Title: 덧칠하기, Time: 1.51 ms, Memory: 77.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.java
@@ -0,0 +1,18 @@
+import java.lang.Math;
+
+class Solution {
+    public int solution(int n, int m, int[] section) {
+        int answer = 1;
+        
+        int start = section[0];
+        
+        for(int i=1; i<section.length; i++) {
+            if(start + m -1 < section[i]) {
+                answer++;
+                start = section[i];
+            }
+        }
+        
+        return answer;
+    }
+}
