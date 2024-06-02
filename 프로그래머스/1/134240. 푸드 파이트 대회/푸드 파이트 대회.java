10f26301ce268d4b53621725283dc4f8f337955c
HyunJun-Kwon <150315701+HyunJun-Kwon@users.noreply.github.com>
2024-06-02 22:49:28 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 11.14 ms, Memory: 73.6 MB -BaekjoonHub

--- 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
@@ -1,37 +1,13 @@
 class Solution {
     public String solution(int[] food) {
-        int totalAmount = 0;
-        int[] evenNumFood = new int[food.length];
+        String answer = "0";
 
-       
-        evenNumFood[0] = food[0];
-
-        for (int i = 1; i < food.length; i++) {
-            if (food[i] % 2 != 0) {
-                evenNumFood[i] = (food[i] - 1) / 2;
-            } else {
-                evenNumFood[i] = food[i] / 2;
-            }
-         
-            totalAmount += evenNumFood[i];
-        }
-        
-        StringBuilder answerBuild = new StringBuilder(totalAmount * 2 + 1);
-
-        for (int i = 1; i < evenNumFood.length; i++) {
-            for (int j = 0; j < evenNumFood[i]; j++) {
-                answerBuild.append(i);
+        for (int i = food.length - 1; i > 0; i--) {
+            for (int j = 0; j < food[i] / 2; j++) {
+                answer = i + answer + i; 
             }
         }
-        
-        answerBuild.append(0);
-        
-        for (int i = evenNumFood.length - 1; i > 0; i--) {
-            for (int j = 0; j < evenNumFood[i]; j++) {
-                answerBuild.append(i);
-            }
-        }
-        
-        return answerBuild.toString();
+
+        return answer;
     }
 }
